51fee65d754679fa1539c447904c68fc737e6c51
Minu <minuphilipchoi@gmail.com>
2019-11-24 14:19:27 +0000

    Added 'condition' of item to the item table.

--- public/html/create_db.sql
+++ public/html/create_db.sql
@@ -97,6 +97,7 @@ CREATE TABLE IF NOT EXISTS `item` (
                                       `description` text NOT NULL,
                                       `size` varchar(30) NOT NULL,
                                       `location` int(16) NOT NULL,
+                                      `condition` varchar(12) NOT NULL,
                                       `category` varchar(40) NOT NULL,
                                       `seller_fk` int(16) NOT NULL,
                                       PRIMARY KEY (`item_id`),
